2cc85b23d12c4754da4257c46b2170454bc16211
maxlath <k@maxlath.eu>
2019-06-01 17:04:56 +0200

    login only if needed

--- wikibase-token.js
+++ wikibase-token.js
@@ -1,14 +1,18 @@
 const login = require('./lib/login')
 const getTokenGetter = require('./lib/get_token_getter')
 
-module.exports = (config) => {
-  if (config.oauth) {
-    const loginCookiesPromise = Promise.resolve('')
-    return getTokenGetter(config, loginCookiesPromise, config.oauth)
-  } else if (config.username && config.password) {
-    const loginCookiesPromise = login(config)
-    return getTokenGetter(config, loginCookiesPromise)
-  } else {
+module.exports = config => {
+  if (!(config.oauth || (config.username && config.password))) {
     throw new Error('no authentification means provided')
   }
+
+  var loginCookiesPromise
+  const getLoginCookies = () => {
+    if (loginCookiesPromise) return loginCookiesPromise
+    if (config.oauth) loginCookiesPromise = Promise.resolve('')
+    else loginCookiesPromise = login(config)
+    return loginCookiesPromise
+  }
+
+  return getTokenGetter(config, getLoginCookies)
 }
